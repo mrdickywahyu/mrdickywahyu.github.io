1f638eba40bb469a41960c476ae068cc93c8e89b
morayya <morayyafaradynia@gmail.com>
2020-06-09 17:14:58 +0700

    portfolio

---
+++ js/scripy.js
@@ -0,0 +1,16 @@
+$('.page-scroll').on('click', function(e) {
+    
+
+
+     var tujuan = $(this).attr('href');
+     
+     var elementTujuan = $(tujuan);
+
+     $('body').animate({
+
+        scrollTop: elementTujuan.offset().top
+     });
+
+     e.preventDefault(); 
+
+});
